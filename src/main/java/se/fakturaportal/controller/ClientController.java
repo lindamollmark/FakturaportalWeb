551b49d4daae3843bd32bb7dc1d761fa506de801
Linda <linda.mollmark@gmail.com>
2016-05-12 22:06:20 +0200

    new comments on client and invoiceController

--- src/main/java/se/fakturaportal/controller/ClientController.java
+++ src/main/java/se/fakturaportal/controller/ClientController.java
@@ -8,50 +8,69 @@ import se.fakturaportal.core.model.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
 /**
  * Class for saving the customers/client for the user.
  */
 @RestController
 public class ClientController {
 
-
     @Autowired
     private ClientService clientService;
 
+    /**
+     * Adds a new client
+     * @param client the client to be saved
+     * @return the saved client
+     */
     @RequestMapping(value = "/views/newClient", method = RequestMethod.POST)
     public String newClient(@RequestBody String client){
         Client aClient = new Gson().fromJson(client, Client.class);
-         aClient = clientService.newClient(aClient);
+        aClient = clientService.newClient(aClient);
         return new Gson().toJson(aClient);
-   }
+    }
 
+    /**
+     * Fetches the clientList from the database
+     * @return the list of clients
+     */
     @RequestMapping(value = "/views/clientlist", method = RequestMethod.GET)
     public String clientList(){
         List<Client> clients = clientService.getClients();
         String json = new Gson().toJson(clients);
         return json;
-
     }
 
+    /**
+     * Gets a specific client
+     * @param clientId the Id for the client to fetch.
+     * @return the client to return for the user.
+     */
     @RequestMapping(value = "/views/clientView", method = RequestMethod.POST)
-    public String clientView(@RequestBody String client){
-       TestId clientID = new Gson().fromJson(client, TestId.class);
+    public String clientView(@RequestBody String clientId){
+        TestId clientID = new Gson().fromJson(clientId, TestId.class);
         Client theClient = clientService.fetchClient(clientID.getClientId());
         String json = new Gson().toJson(theClient);
         return json;
     }
 
+    /**
+     * Deletes a specific client, if a client has invoices, you can't delete them.
+     * @param clientId for the client to be deleted
+     * @return a boolean (true/false) if the client has been deleted or not.
+     */
     @RequestMapping(value = "/views/deleteClient", method = RequestMethod.POST)
-    public boolean deleteClient(@RequestBody String client){
-        TestId aClient = new Gson().fromJson(client, TestId.class);
+    public boolean deleteClient(@RequestBody String clientId){
+        TestId aClient = new Gson().fromJson(clientId, TestId.class);
         Boolean deleated = clientService.deleteClient(aClient.getClientId());
         return deleated;
     }
 
+    /**
+     * Updates the information on a client
+     * @param client the clientinformation.
+     * @return the updated client
+     */
     @RequestMapping(value = "/views/updateClient", method = RequestMethod.POST)
     public String updateClient(@RequestBody String client){
         Client aClient = new Gson().fromJson(client, Client.class);
@@ -60,10 +79,15 @@ public class ClientController {
         return json;
     }
 
+    /**
+     * Method to investigate if a clientNo is avalible or not.
+     * If someone else already has the clientNo you can't make another client with the same number.
+     * @param clientNo the number you wish to give your new client.
+     * @return true if the number is free to use, false if it's already taken.
+     */
     @RequestMapping(value = "views/clientNo", method = RequestMethod.POST)
     public Boolean checkClientNo(@RequestBody String clientNo){
         Boolean exist = clientService.checkClientNo(Integer.parseInt(clientNo));
-
         return exist;
     }
 }
--- src/main/java/se/fakturaportal/controller/InvoiceController.java
+++ src/main/java/se/fakturaportal/controller/InvoiceController.java
@@ -8,60 +8,105 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import se.fakturaportal.core.model.Invoice;
-import se.fakturaportal.core.model.InvoiceId;
 import se.fakturaportal.core.model.TestId;
 import se.fakturaportal.core.service.InvoiceService;
 
 import java.util.List;
 
+/**
+ * Class to handle the invoices
+ */
 @RestController
 public class InvoiceController {
 
     @Autowired
     private InvoiceService invoiceService;
 
+    /**
+     * Method to save the new invoice.
+     * @param invoice the information to save
+     * @return the saved invoice
+     */
     @RequestMapping(value = "/views/newInvoice", method = RequestMethod.POST)
     public String newInvoice(@RequestBody String invoice){
-        Invoice newInvoice = new Gson().fromJson(invoice, Invoice.class);
+        Invoice newInvoice = invoiceFromJson(invoice);
         newInvoice = invoiceService.newInvoice(newInvoice);
-        String json = new Gson().toJson(newInvoice);
-        return json;
-    }
-    @RequestMapping(value="/views/fetchInvoice", method = RequestMethod.POST)
-    public String fetchInvoice(@RequestBody String invoiceNo){
-        Invoice invoice = new Gson().fromJson(invoiceNo, Invoice.class);
-         invoice = invoiceService.fetchInvoice(invoice);
-        String json = new Gson().toJson(invoice);
+        String json = invoiceToJson(newInvoice);
         return json;
     }
 
+    /**
+     * Help method to make a Json format of a invoice
+     * @param invoice the invoice to format
+     * @return the Json string
+     */
+    private String invoiceToJson(Invoice invoice) {
+        return new Gson().toJson(invoice);
+    }
+
+    /**
+     * Help method to make an invoice from a Json
+     * @param invoice the Json string with the invoice information
+     * @return the invoice
+     */
+    private Invoice invoiceFromJson(String invoice) {
+        return new Gson().fromJson(invoice, Invoice.class);
+    }
+
+    /**
+     * Method to fetch a invoice from the database
+     * @param invoiceNo the invoice number you want to receive
+     * @return the invoice in Json format.
+     */
+    @RequestMapping(value="/views/fetchInvoice", method = RequestMethod.POST)
+    public String fetchInvoice(@RequestBody String invoiceNo){
+        Invoice invoice = invoiceFromJson(invoiceNo);
+        invoice = invoiceService.fetchInvoice(invoice);
+        String json = invoiceToJson(invoice);
+        return json;
+    }
+
+    /**
+     * Gets the next invoiceNumber to use.
+     * @return next avalible invoicenumber.
+     */
     @RequestMapping(value = "/views/newInvoiceNo", method = RequestMethod.GET)
     public int getInvoiceNo(){
         int invoiceNo = invoiceService.getInvoiceNo();
-
         return invoiceNo;
     }
 
+    /**
+     * Fetches all the invoices from the database
+     * @return Json string with all the invoices.
+     */
     @RequestMapping(value = "/views/invoicelist", method = RequestMethod.GET)
     public String invoiceList() {
         List<Invoice> invoices = invoiceService.fetchInvoiceList();
-
         String json = new Gson().toJson(invoices);
         return json;
     }
 
+    /**
+     * Fetches the invoices for a specific client
+     * @param clientId the Id for the client we want the invoices for.
+     * @return the list of invoices in a Json string
+     */
     @RequestMapping(value = "/views/clientInvoicelist", method = RequestMethod.POST)
-    public String clientInvoiceList(@RequestBody String client) {
-        TestId clientID = new Gson().fromJson(client, TestId.class);
+    public String clientInvoiceList(@RequestBody String clientId) {
+        TestId clientID = new Gson().fromJson(clientId, TestId.class);
         List<Invoice> invoices = invoiceService.fetchClientInvoiceList(clientID);
-
         String json = new Gson().toJson(invoices);
         return json;
     }
 
+    /**
+     * Method to update the invoice.
+     * @param invoiceToUpdate the invoice with the information that shall be updated.
+     */
     @RequestMapping(value= "views/updateInvoice", method = RequestMethod.POST)
     public void updateInvoice(@RequestBody String invoiceToUpdate){
-        Invoice invoice = new Gson().fromJson(invoiceToUpdate, Invoice.class);
+        Invoice invoice = invoiceFromJson(invoiceToUpdate);
         invoiceService.updateInvoice(invoice);
     }
 }
